26ee5989ee1521857cf1ad552eac55157f6d6394
Isabel Enriquez <102576143+izzyenrii@users.noreply.github.com>
2022-03-31 21:42:48 -0700

    Coding Journal 1 Session Prompt 1

---
+++ cj1-1.py
@@ -0,0 +1,19 @@
+#this program will write
+#Session 1 prompt
+
+
+#This function tells the 
+#computer to print
+#"My name, pronouns, favorite movie and food"
+def printSession1Prompt():
+	print("My name is Isabel Enriquez. My pronouns are she/her. My favorite movie is Stuck in Love. My favorite food is poke.")
+
+#This defines our main()
+#function for our program
+def main():
+    printSession1Prompt()
+
+#When we run the program
+#this executes first
+if __name__=="__main__":
+    main()
