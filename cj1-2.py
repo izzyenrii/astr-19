224c6e53b445514ed07ca847751973843fe8db47
Isabel Enriquez <102576143+izzyenrii@users.noreply.github.com>
2022-04-02 20:19:43 -0700

    Journal Prompt 2 of Code Journal 1

---
+++ cj1-2.py
@@ -0,0 +1,31 @@
+#this program will print
+#the sum of two floating point numbers,
+#the difference between two integers,
+#and and the product of a floating point number
+#and an integer
+
+#n1 and n2 will combine to find the sum
+n1 = 4.27
+n2 = 10.3
+#add the sum of two floating numbers
+n = n1 + n2
+#display the sum
+print(n)
+
+#n3 and n4 will produce the difference between two integers
+n3 = 20
+n4 = 14
+#find the difference between the two integers
+n = (n4 - n3)
+#display the difference
+print(n)
+
+#n5 and n6 will produce the product of a 
+#floating point number and an integer
+n5 = 2.2
+n6 = 10
+#find the product between floating point
+#number and the integer
+n = (n5 * n6)
+#display the product
+print(n)
